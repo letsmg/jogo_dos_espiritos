9710f240e372381e9bb35fcc711ea621b50fa0d0
Luiz Eduardo <luiztsilva@protonmail.com>
2026-04-10 16:54:50 -0300

    correcoes diversas e exclusao de lixo

--- git-status-check.js
+++
@@ -1,9 +0,0 @@
-import { execSync } from 'child_process';
-
-try {
-  console.log('Verificando status do Git...');
-  const status = execSync('git status', { encoding: 'utf8' });
-  console.log(status);
-} catch (error) {
-  console.error('Erro ao verificar status do Git:', error.message);
-}
